d0f0623763b952a5355b75aa6c4d4b4106abe6d7
Eldred Habert <eldredhabert0@gmail.com>
2021-06-13 13:16:43 +0200

    Un-hardcode stack location
    It does not matter where in WRAM the stack is, so don't hardcode what doesn't need to be!

--- src/header.asm
+++ src/header.asm
@@ -143,10 +143,9 @@ wShadowOAM::
 	ds NB_SPRITES * 4
 
 
-FAIL "If not using banked WRAM, then replace $D000 with $E000 and delete this line"
-; This ensures that the stack is at the very end of WRAM
-SECTION "Stack", WRAM0[$D000 - STACK_SIZE]
+SECTION "Stack", WRAM0
 
+wStack:
 	ds STACK_SIZE
 wStackBottom:
 
